fd74abc3e6a3b1ac9cd2c6db3884afb5afd214ba
Roland Stedy <stedyroland@gmail.com>
2024-08-25 04:51:50 +0300

    Finished

--- script.js
+++ script.js
@@ -7,9 +7,32 @@ function addNote() {
     if (newnote.value==='') {
     alert('You need to add a new note')
   } else{
-    let para = document.createElement("li");
-    para.innerHTML = newnote.value;
-    contentBox.appendChild(para);
+    let li = document.createElement("li");
+    li.innerHTML = newnote.value;
+    contentBox.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
   }
   newnote.value="";
+  storeData()
 }
+
+contentBox.addEventListener("click", (e)=>{
+  if (e.target.tagName=== "LI") {
+    e.target.classList.toggle('checked');
+    storeData()
+  } else if (e.target.tagName=== "SPAN"){
+    e.target.parentElement.remove();
+    storeData()
+  }
+})
+
+function storeData() {
+  localStorage.setItem("data", contentBox.innerHTML);
+}
+
+function getData() {
+  contentBox.innerHTML=localStorage.getItem("data")
+}
+getData()
